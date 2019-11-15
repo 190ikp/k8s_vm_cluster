6db9e5fa501cc9c57f824e652e85537d9c61a5c9
190ikp <red.volovo1996@outlook.com>
2019-11-15 22:45:56 +0900

    set hostname

--- Vagrantfile
+++ Vagrantfile
@@ -17,16 +17,23 @@ Vagrant.configure("2") do |config|
         master.vm.network "private_network", ip: "10.10.10.1#{i}", virtualbox__intnet: "k8s"
         master.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1#{i}"
         master.vm.network "forwarded_port", guest: 443, host: 8443, host_ip: "127.0.0.1#{i}"
+        master.vm.provision "shell", inline: <<-SHELL
+          export HOSTNAME
+          HOSTNAME=master-#{i}
+          /vagrant/node_init.sh all
+        SHELL
       end
 
       config.vm.define "worker-#{i}" do |worker|
         worker.vm.network "private_network", ip: "10.10.10.2#{i}", virtualbox__intnet: "k8s"
         worker.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.2#{i}"
         worker.vm.network "forwarded_port", guest: 443, host: 8443, host_ip: "127.0.0.2#{i}"
+        worker.vm.provision "shell", inline: <<-SHELL
+          export HOSTNAME
+          HOSTNAME=worker-#{i}
+          /vagrant/node_init.sh all
+        SHELL
       end
     end
-
-    config.vm.provision "shell", inline: "/vagrant/node_init.sh all"
-
   end
   
--- node_init.sh
+++ node_init.sh
@@ -3,69 +3,72 @@ set -euo pipefail
 
 
 setup_packages() {
-    
-    sudo add-apt-repository universe
-    sudo apt update
-    sudo apt upgrade --yes
 
-    sudo apt install --yes \
-        build-essential \
-        linux-headers-"$(uname -r)" \
-        tmux
+  sudo add-apt-repository universe
+  sudo apt update
+  sudo debconf-set-selections <<< 'libssl1.1:amd64 libraries/restart-without-asking: true'
+  sudo apt upgrade --yes
 
+  sudo apt install --yes \
+    build-essential \
+    linux-headers-"$(uname -r)"
+}
+
+setup_network() {
+  envsubst \$HOSTNAME < /vagrant/conf/hosts |
+    sudo dd of=/etc/hosts
 }
 
 setup_docker() {
 
-    sudo apt install --yes \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg-agent \
-        software-properties-common
+  sudo apt install --yes \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) \
-        stable"
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
 
-    sudo apt update
-    # installed version is 18.09 for k8s support
-    sudo apt install --yes \
-        docker-ce=5:18.09.9~3-0~ubuntu-bionic \
-        docker-ce-cli=5:18.09.9~3-0~ubuntu-bionic
-    sudo apt-mark hold docker-ce docker-ce-cli
-
+  sudo apt update
+  # installed version is 18.09 for k8s support
+  sudo apt install --yes \
+    docker-ce=5:18.09.9~3-0~ubuntu-bionic \
+    docker-ce-cli=5:18.09.9~3-0~ubuntu-bionic
+  sudo apt-mark hold docker-ce docker-ce-cli
 }
 
 setup_k8s() {
 
-    setup_docker
+  setup_docker
 
-    # default cgroup driver is cgroups is cgroupfs, but recommended driver is systemd.
-    # follow the guide at https://kubernetes.io/docs/setup/cri/ (too complex)
-    # echo 'plugins.cri.systemd_cgroup = true' |
-    #     sudo tee -a /etc/containerd/config.toml
+  # default cgroup driver is cgroups is cgroupfs, but recommended driver is systemd.
+  # follow the guide at https://kubernetes.io/docs/setup/cri/ (too complex)
+  # echo 'plugins.cri.systemd_cgroup = true' |
+  #     sudo tee -a /etc/containerd/config.toml
 
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-    echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' |
-        sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  echo 'deb https://apt.kubernetes.io/ kubernetes-xenial main' |
+    sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 
-    sudo apt update
-    sudo apt install --yes \
-        kubelet=1.16
-    sudo apt-mark hold \
-        kubelet
-    
+  sudo apt update
+  sudo apt install --yes \
+    kubelet
+  sudo apt-mark hold \
+    kubelet
 }
 
 all() {
 
-    setup_packages
-    setup_k8s
+  setup_packages
+  setup_network
+  setup_k8s
 
-    echo 'All setup is finished.'
+  echo 'All setup is finished.'
 }
 
 eval "$1"
