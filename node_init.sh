e6b6ec9e969306c83f4596cdd35a786ca16cf2bd
190ikp <red.volovo1996@outlook.com>
2019-11-17 23:35:36 +0900

    fix bug in skipping restart prompt

--- node_init.sh
+++ node_init.sh
@@ -6,7 +6,7 @@ setup_packages() {
 
   sudo add-apt-repository universe
   sudo apt update
-  sudo debconf-set-selections <<< 'libssl1.1:amd64 libraries/restart-without-asking: true'
+  sudo debconf-set-selections <<< 'libssl1.1:amd64 libraries/restart-without-asking boolean true'
   sudo apt upgrade --yes
 
   sudo apt install --yes \
